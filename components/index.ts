36313bfdc62e876cd6cdaa719726384b5d671230
dkdlelw <1346zany@gmail.com>
2023-07-20 18:11:08 +0900

    0720

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,18 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManuFacturer from "./SearchManuFacturer";
+import CarCard from "./CarCard";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManuFacturer,
+  CarCard,
+};
